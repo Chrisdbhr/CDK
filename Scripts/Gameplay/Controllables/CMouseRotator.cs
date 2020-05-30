568505a7670339382b61da04e50a7abe2bbb3b99
Chrisdbhr <chrisdbhr@outlook.com>
2020-05-30 20:16:12 -0300

    CMouseRotator now with Horizontal rotation

--- Scripts/Gameplay/Controllables/CMouseRotator.cs
+++ Scripts/Gameplay/Controllables/CMouseRotator.cs
@@ -5,15 +5,19 @@ namespace CDK {
 	public class CMouseRotator : MonoBehaviour {
 		#region <<---------- Properties and Fields ---------->>
 		[SerializeField] private Vector2 _rotationSpeed = Vector2.one * 0.2f;
-		[SerializeField] private Vector2 _rotationXRange = new Vector2(40f, 50f);
+		[SerializeField] private Vector2 _rotationYRange = new Vector2(60f, 60f);
+		[SerializeField] private Vector2 _rotationXRange = new Vector2(60f, 30f);
 		[NonSerialized] private Vector3 _eulerRotation;
 		[NonSerialized] private Vector2 _inputLook;
 		[NonSerialized] private Transform _transform;
+		[NonSerialized] private Quaternion _initialRotation;
+		
 		#endregion <<---------- Properties and Fields ---------->>
 
 		#region <<---------- MonoBehaviour ---------->>
 		protected void Awake() {
 			this._transform = this.transform;
+			this._initialRotation = this._transform.rotation;
 		}
 
 		private void Update() {
@@ -33,12 +37,24 @@ namespace CDK {
 			// clamp rotation
 			this._eulerRotation = this._transform.eulerAngles;
 			this._eulerRotation.z = 0f;
+			// vertical rotation
 			if (this._eulerRotation.x.CIsInRange(this._rotationXRange.x, 360 - this._rotationXRange.y)) {
 				this._eulerRotation.x = this._eulerRotation.x.CGetCloserValue(this._rotationXRange.x, 360 - this._rotationXRange.y);
+			}
+			
+			// horizontal rotation
+			if (this._eulerRotation.y.CIsInRange(this._rotationYRange.x, 360 - this._rotationYRange.y)) {
+				this._eulerRotation.y = this._eulerRotation.y.CGetCloserValue(this._rotationYRange.x, 360 - this._rotationYRange.y);
 			}
 			this._transform.eulerAngles = this._eulerRotation;
 		}
 		#endregion <<---------- MonoBehaviour ---------->>
 
+
+
+		public void ResetRotation() {
+			this._transform.rotation = this._initialRotation;
+		}
+		
 	}
 }
