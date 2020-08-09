9e7ae08d80dec535d2fd08689bdffcde74ccf793
Chrisdbhr <chrisdbhr@outlook.com>
2020-08-09 15:34:24 -0300

    interaction base initial layer is default

--- Scripts/Interaction/PlayerInteraction/CPlayerInteractionBase.cs
+++ Scripts/Interaction/PlayerInteraction/CPlayerInteractionBase.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 namespace CDK {
 	public abstract class CPlayerInteractionBase : MonoBehaviour {
 		
-		[SerializeField] protected LayerMask _interactionLayerMask;
+		[SerializeField] protected LayerMask _interactionLayerMask = 1;
 		[NonSerialized] protected Transform _myTransform;
 		
 		protected virtual void Awake() {
