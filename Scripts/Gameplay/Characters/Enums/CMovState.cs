08d6dff8079f2b02c2fd2445056dd8e92558d0a6
Chrisdbhr <chrisdbhr@outlook.com>
2020-12-28 18:36:27 -0300

    improved movstate

--- Scripts/Gameplay/Characters/Enums/CMovState.cs
+++ Scripts/Gameplay/Characters/Enums/CMovState.cs
@@ -1,13 +1,12 @@
 namespace CDK {
 	public enum CMovState {
-		Idle, Walking, Running, Sliding
+		Idle, Walking, Running, Sprint, Sliding
 	}
 	
 	public static class CMovStateExtensions {
 		public static bool IsMoving(this CMovState movState) {
-			return movState == CMovState.Running 
-					|| movState == CMovState.Walking
-					|| movState == CMovState.Sliding;
+			return movState > CMovState.Idle;
 		}
+
 	}
 }
