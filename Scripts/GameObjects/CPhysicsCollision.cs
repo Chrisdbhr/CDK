ecf0a43dca1af186708693e2a8d289c7252a1181
Chrisdbhr <chrisdbhr@outlook.com>
2021-02-23 02:31:30 -0300

    physics collision compatible with Players

--- Scripts/GameObjects/CPhysicsCollision.cs
+++ Scripts/GameObjects/CPhysicsCollision.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 namespace CDK {
 	public class CPhysicsCollision : MonoBehaviour {
 
+		[SerializeField] private bool TriggeredByPlayerOnly = false;
+
 		[SerializeField] private CUnityEventTransform CollisionEnter;
 		[SerializeField] private CUnityEventTransform CollisionExit;
 
@@ -10,19 +12,22 @@ namespace CDK {
 		
 		
 		private void OnCollisionEnter(Collision other) {
-			Debug.Log($"{this.name} collided with {other.transform.name}");
+			if (this.TriggeredByPlayerOnly && !CGamePlayerManager.get.IsRootTransformFromAPlayerCharacter(other.transform)) return;
 			this.CollisionEnter?.Invoke(other.transform);
 		}
 
 		private void OnCollisionExit(Collision other) {
+			if (this.TriggeredByPlayerOnly && !CGamePlayerManager.get.IsRootTransformFromAPlayerCharacter(other.transform)) return;
 			this.CollisionExit?.Invoke(other.transform);
 		}
 
 		private void OnCollisionEnter2D(Collision2D other) {
+			if (this.TriggeredByPlayerOnly && !CGamePlayerManager.get.IsRootTransformFromAPlayerCharacter(other.transform)) return;
 			this.CollisionEnter?.Invoke(other.transform);
 		}
 
 		private void OnCollisionExit2D(Collision2D other) {
+			if (this.TriggeredByPlayerOnly && !CGamePlayerManager.get.IsRootTransformFromAPlayerCharacter(other.transform)) return;
 			this.CollisionExit?.Invoke(other.transform);
 		}
 	}
