68a4718f86139036eb5c32669f79c5b6ec80ee60
Chrisdbhr <chrisdbhr@outlook.com>
2021-02-23 02:28:02 -0300

    improved triggers

--- Scripts/Events/CBlockingEventsTrigger.cs
+++ Scripts/Events/CBlockingEventsTrigger.cs
@@ -1,7 +1,59 @@
+using System;
 using UnityEngine;
 
 namespace CDK {
-	public class BlockingEventsTrigger : MonoBehaviour {
+	public class CBlockingEventsTrigger : MonoBehaviour {
+
+		#region <<---------- Properties and Fields ---------->>
+		
+		[SerializeField] private CUnityEventBool _isBlockingEvent;
+		[SerializeField] private CUnityEventBool _onCutsceneEvent;
+		[SerializeField] private CUnityEventBool _onMenuEvent;
+
+		#endregion <<---------- Properties and Fields ---------->>
+
+		
+		
+		
+		#region <<---------- MonoBehaviour ---------->>
+
+		private void OnEnable() {
+			CBlockingEventsManager.BlockingEventHappeningEvent += this.OnBlockingEvent;
+			CBlockingEventsManager.PlayingCutsceneEvent += this.OnCutsceneEvent;
+			CBlockingEventsManager.OnMenuEvent += this.OnMenuEvent;
+		}
+
+		private void OnDisable() {
+			CBlockingEventsManager.BlockingEventHappeningEvent -= this.OnBlockingEvent;
+			CBlockingEventsManager.PlayingCutsceneEvent -= this.OnCutsceneEvent;
+			CBlockingEventsManager.OnMenuEvent -= this.OnMenuEvent;
+		}
+		
+		#endregion <<---------- MonoBehaviour ---------->>
+
+		
+		
+		
+		#region <<---------- Events ---------->>
+
+		private void OnBlockingEvent(bool isBlocking) {
+			this._isBlockingEvent?.Invoke(isBlocking);
+		}
+		
+		private void OnCutsceneEvent(bool onCutscene) {
+			this._onCutsceneEvent?.Invoke(onCutscene);
+		}
+		
+		private void OnMenuEvent(bool onMenu) {
+			this._onMenuEvent?.Invoke(onMenu);
+		}
+		
+		#endregion <<---------- Events ---------->>
+
+
+
+
+		#region <<---------- Triggers ---------->>
 
 		public void TriggerIsOnMenuBlockingEvent(bool block = true) {
 			CBlockingEventsManager.IsOnMenu = block;
@@ -11,5 +63,6 @@ namespace CDK {
 			CBlockingEventsManager.IsPlayingCutscene = block;
 		}
 		
+		#endregion <<---------- Triggers ---------->>
 	}
 }
