72d60b6689e5d68fdac34cdc89cf8bcb8d52252c
Chrisdbhr <chrisdbhr@outlook.com>
2021-02-23 02:30:38 -0300

    cursor manager

---
+++ Scripts/System/CCursorManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace CDK {
+	public static class CCursorManager {
+		
+		/// <summary>
+		/// ANTES da scene load.
+		/// </summary>
+		[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+		private static void Initialize() {
+			SetCursorState(!CGameSettings.get.CursorStartsHidden);
+			CBlockingEventsManager.OnMenuEvent += SetCursorState;
+		}
+
+
+		private static void SetCursorState(bool visible) {
+			Cursor.visible = visible;
+			
+			if (visible) {
+				Cursor.lockState = CursorLockMode.None;
+			}
+			else {
+				Cursor.lockState = CursorLockMode.Locked;
+			}
+		}
+
+	}
+}
