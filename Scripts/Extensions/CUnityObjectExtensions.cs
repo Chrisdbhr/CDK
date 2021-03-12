3dd753824953ffbaf2fb7b04de879c5ebc56dd31
Chrisdbhr <chrisdbhr@outlook.com>
2021-03-12 06:02:03 -0300

    extensions

--- Scripts/Extensions/CUnityObjectExtensions.cs
+++ Scripts/Extensions/CUnityObjectExtensions.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 namespace CDK {
 	public static class CUnityObjectExtensions {
-		public static void CDestroy(this Object value) {
+		public static void CDestroy(this Object value, float time = 0f) {
 			if (value == null) return;
 			if (Application.isPlaying) {
-				Object.Destroy(value);
+				Object.Destroy(value, time > 0f ? time : 0f);
 			}
 			else {
 				Object.DestroyImmediate(value);
