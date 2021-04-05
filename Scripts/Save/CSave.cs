1c5b4b515d2e7aa0d36d3f960765e343e71a2511
Chris <chrisdbhr@gmail.com>
2021-04-05 03:13:53 -0300

    Save system WIP

--- Scripts/Save/CSave.cs
+++ Scripts/Save/CSave.cs
@@ -1,4 +1,7 @@
 using System;
+using System.IO;
+using System.Threading.Tasks;
+using UniRx;
 using UnityEngine;
 using UnityJSON;
 
@@ -23,19 +26,96 @@ namespace CDK {
 		#endregion <<---------- Singleton ---------->>
 
 
+		#region <<---------- Properties ---------->>
+		
+		[JSONNode(NodeOptions.ReplaceDeserialized, key = KEY_CameraSensitivityHorizontal)]
+		public readonly ReactiveProperty<float> CameraSensitivityHorizontal = new ReactiveProperty<float>(5f);
+		public const string KEY_CameraSensitivityHorizontal = "cameraSensitivityHorizontal";
+		
+		[JSONNode(NodeOptions.ReplaceDeserialized, key = KEY_CameraSensitivityVertical)]
+		public readonly ReactiveProperty<float> CameraSensitivityVertical = new ReactiveProperty<float>(0.1f);
+		public const string KEY_CameraSensitivityVertical = "cameraSensitivityVertical";
+
+		#endregion <<---------- Properties ---------->>
 
 
-		public float CameraSensitivity {
-			get { return this._cameraSensitivity; }
-			set {
-				if (this._cameraSensitivity == value) return;
-				this.CameraSensitivity_Changed?.Invoke(this._cameraSensitivity = value);
+		#region <<---------- Saving ---------->>
+
+		public static async Task SaveGame() {
+			try {
+				Debug.Log($"Starting SaveGame process.");
+
+				var json = CJsonUtils.DefaultSerializer.Serialize(get);
+			
+				var folderPath = GetSaveFileDirectory();
+				if (!Directory.Exists(folderPath)) {
+					Directory.CreateDirectory(folderPath);
+				}
+				var filePath = GetSaveFilePath();
+			
+				Debug.Log($"Trying to SaveGame in file '{filePath}'");
+
+				using var streamWriter = File.CreateText(filePath);
+				await streamWriter.WriteAsync(json);
+			
+				Debug.Log($"Game saved.");
+			}
+			catch (Exception e) {
+				Debug.LogError(e);
 			}
 		}
-		[JSONNode(NodeOptions.SerializeNull | NodeOptions.ReplaceDeserialized | NodeOptions.IgnoreDeserializationTypeErrors, key = "cameraSensitivity")]
-		private float _cameraSensitivity = 1f;
-		public event Action<float> CameraSensitivity_Changed;
+		
+		#endregion <<---------- Saving ---------->>
 
 
+		
+		#region <<---------- Loading ---------->>
+
+		public static async Task LoadGame() {
+			try {
+				var filePath = GetSaveFilePath();
+				
+				Debug.Log($"Trying to LoadGame with file '{filePath}'");
+
+				if (!File.Exists(filePath)) {
+					Debug.LogWarning($"Save file at path '{filePath}' doesnt exist!");
+					return;
+				}
+
+				var fileContent = File.ReadAllText(filePath);
+				
+				Debug.Log($"Save file content: {fileContent}");
+
+				var save = CJsonUtils.DefaultDeserializer.Deserialize<CSave>(fileContent);
+
+				if (save == null) {
+					Debug.LogError($"Could not deserialize Save at path '{filePath}'!");
+					return;
+				}
+
+				CSave._instance = save;
+				
+				Debug.Log($"Game Loaded.");
+			}
+			catch (Exception e) {
+				Debug.LogError(e);
+			}
+		}
+
+		#endregion <<---------- Loading ---------->>
+		
+		
+		
+		#region <<---------- Path ---------->>
+
+		private static string GetSaveFileDirectory() {
+			return Path.Combine(Application.persistentDataPath, "SaveGame").Replace('\\','/');
+		}
+		private static string GetSaveFilePath() {
+			return GetSaveFileDirectory() + "/gamesave.json";
+		}
+
+		#endregion <<---------- Path ---------->>
+		
 	}
 }
---
+++ Scripts/Save/Serialization/CSaveSerializer.cs
@@ -0,0 +1,7 @@
+namespace CDK {
+	public class CSaveSerializer {
+
+		
+
+	}
+}
