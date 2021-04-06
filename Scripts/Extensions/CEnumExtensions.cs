3754e5c70ee22217abf003ead04a8d742ce360ef
Chris <chrisdbhr@gmail.com>
2021-04-06 18:58:36 -0300

    ToInt enum extension

--- Scripts/Extensions/CEnumExtensions.cs
+++ Scripts/Extensions/CEnumExtensions.cs
@@ -8,6 +8,10 @@ namespace CDK {
 		public static IEnumerable<T> GetValues<T>() {
 			return Enum.GetValues(typeof(T)).Cast<T>();
 		}
+		
+		public static int ToInt(this Enum @enum) {
+			return Convert.ToInt32(@enum);
+		}
 
 	}
 }
