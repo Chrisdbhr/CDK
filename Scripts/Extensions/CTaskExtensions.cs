d46f7c32b87e9a58d808ad5845b544a087db2084
Chris <chrisdbhr@gmail.com>
2021-06-07 10:08:57 -0300

    fix task extension returning a Task instead of void

--- Scripts/Extensions/CTaskExtensions.cs
+++ Scripts/Extensions/CTaskExtensions.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 namespace CDK {
 	public static class CTaskExtensions {
 
-		public static async Task CAwait(this Task task, bool continueOnCapturedContext = true) {
+		public static async void CAwait(this Task task, bool continueOnCapturedContext = true) {
 			await task.ConfigureAwait(continueOnCapturedContext);
 		}
 		
