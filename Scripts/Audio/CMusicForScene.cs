3b65be6c70e837cfe61416875ff744a2be920f95
Chris <chrisdbhr@gmail.com>
2021-06-07 10:07:52 -0300

    improved FMOD music for scene play

--- Scripts/Audio/CMusicForScene.cs
+++ Scripts/Audio/CMusicForScene.cs
@@ -5,25 +5,27 @@ using UnityEngine.SceneManagement;
 
 namespace CDK.Audio {
 	public class CMusicForScene : MonoBehaviour {
+	
 		[SerializeField] [EventRef] private string _music;
 		[NonSerialized] private Scene _sceneToCheck;
 		[NonSerialized] private FMOD.Studio.EventInstance _musicState;
 		
-		
-		private void Awake() {
-			this._sceneToCheck = this.gameObject.scene;
-			SceneManager.sceneUnloaded += unloadedScene => {
-				if (unloadedScene != this._sceneToCheck) return;
-				if (!this._musicState.isValid()) return;
-				this._musicState.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
-				this.gameObject.CDestroy(15f);
-			};
-			DontDestroyOnLoad(this);
-		}
 
-		private void Start() {
+		
+		
+		private void OnEnable() {
+			if (this._music.CIsNullOrEmpty()) {
+				Debug.LogWarning($"{this.name} is not referencing a valid Music (EventRef)!");
+				return;
+			}
 			this._musicState = FMODUnity.RuntimeManager.CreateInstance(this._music);
 			if(this._musicState.isValid()) this._musicState.start();
 		}
+		
+		private void OnDisable() {
+			this._musicState.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+
+		}
+		
 	}
 }
