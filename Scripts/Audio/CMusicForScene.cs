b055f046e83875e556ae1b55bc35313b05d4ac60
Chris <chrisdbhr@gmail.com>
2021-06-28 13:05:15 -0300

    fmod music for scene improv

--- Scripts/Audio/CMusicForScene.cs
+++ Scripts/Audio/CMusicForScene.cs
@@ -2,6 +2,7 @@ using System;
 using FMODUnity;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using STOP_MODE = FMOD.Studio.STOP_MODE;
 
 namespace CDK.Audio {
 	public class CMusicForScene : MonoBehaviour {
@@ -9,23 +10,33 @@ namespace CDK.Audio {
 		[SerializeField] [EventRef] private string _music;
 		[NonSerialized] private Scene _sceneToCheck;
 		[NonSerialized] private FMOD.Studio.EventInstance _musicState;
-		
 
-		
-		
+
+
+
+
+		#region <<---------- MonoBehaviour ---------->>
+
 		private void OnEnable() {
 			if (this._music.CIsNullOrEmpty()) {
 				Debug.LogWarning($"{this.name} is not referencing a valid Music (EventRef)!");
 				return;
 			}
-			this._musicState = FMODUnity.RuntimeManager.CreateInstance(this._music);
+			this._musicState = RuntimeManager.CreateInstance(this._music);
 			if(this._musicState.isValid()) this._musicState.start();
 		}
 		
 		private void OnDisable() {
-			this._musicState.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+			this._musicState.stop(STOP_MODE.ALLOWFADEOUT);
 
 		}
 		
+		#if UNITY_EDITOR
+		private void Reset() {
+			this.name = "BGM";
+		}
+		#endif
+		
+		#endregion <<---------- MonoBehaviour ---------->>
 	}
 }
