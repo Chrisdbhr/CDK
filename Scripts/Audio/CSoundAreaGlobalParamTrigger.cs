a1753c51e6fdd1e9ac566986c7594b789cc862dc
Chris <chrisdbhr@gmail.com>
2021-06-28 12:52:37 -0300

    FMOD audio parameters "bool" trigger

---
+++ Scripts/Audio/CSoundAreaGlobalParamTrigger.cs
@@ -0,0 +1,19 @@
+﻿using FMODUnity;
+using UnityEngine;
+
+namespace CDK.Audio {
+	public class CSoundAreaGlobalParamTrigger : MonoBehaviour {
+
+		[SerializeField] [ParamRef] private string _parameter;
+		
+
+		public void TriggerParameter() {
+			FMODUnity.RuntimeManager.StudioSystem.setParameterByName(_parameter, 1f);
+		}
+		
+		public void UntriggerParameter() {
+			FMODUnity.RuntimeManager.StudioSystem.setParameterByName(_parameter, 0f);
+		}
+		
+	}
+}
