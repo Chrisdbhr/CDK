94f2de3d491903b82fbc597c61b097b3f1f77fda
Chris <chrisdbhr@gmail.com>
2021-06-28 12:54:23 -0300

    layermask extension

---
+++ Scripts/Extensions/CLayerMaskExtensions.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace CDK {
+	public static class CLayerMaskExtensions {
+		public static bool Contains(this LayerMask self, LayerMask anotherLayer) {
+			return (self & (1 << anotherLayer)) != 0;
+		}
+	}
+}
