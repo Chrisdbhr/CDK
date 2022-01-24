47bfa5d520174811c6ef8ef6a10f87379b3b967d
Chris <chrisdbhr@gmail.com>
2022-01-24 16:03:59 -0300

    fix enum extensions prefix

--- Scripts/Extensions/CEnumExtensions.cs
+++ Scripts/Extensions/CEnumExtensions.cs
@@ -5,11 +5,11 @@ using System.Linq;
 namespace CDK {
 	public static class CEnumExtensions {
 
-		public static IEnumerable<T> GetValues<T>() {
+		public static IEnumerable<T> CGetValues<T>() {
 			return Enum.GetValues(typeof(T)).Cast<T>();
 		}
 		
-		public static int ToInt(this Enum @enum) {
+		public static int CToInt(this Enum @enum) {
 			return Convert.ToInt32(@enum);
 		}
 
