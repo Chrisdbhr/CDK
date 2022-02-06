1eabe00ac24ad093c99594954e55b491038f9f5c
Chris <chrisdbhr@gmail.com>
2022-02-06 16:14:41 -0300

    gameobject rotator improved performance

--- Scripts/GameObjects/Rotation/CGameObjectRotator.cs
+++ Scripts/GameObjects/Rotation/CGameObjectRotator.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace CDK {
 	public class CGameObjectRotator : CMonoBehaviourUpdateExecutionLoopTime {
@@ -6,10 +7,21 @@ namespace CDK {
 		#region <<---------- Properties and Fields ---------->>
 
 		[SerializeField] private Vector3 _rotateDirectionAndSpeed;
+		[SerializeField] private bool _onlyUpdateWhenVisible = true;
+		[NonSerialized] private bool _isVisible;
 		
 		#endregion <<---------- Properties and Fields ---------->>
-		
+
+		private void OnBecameVisible() {
+			this._isVisible = true;
+		}
+
+		private void OnBecameInvisible() {
+			this._isVisible = false;
+		}
+
 		protected override void Execute(float deltaTime) {
+			if (this._onlyUpdateWhenVisible && !this._isVisible) return;
 			this.transform.Rotate(this._rotateDirectionAndSpeed * (CTime.DeltaTimeScaled));
 		}
 	}
