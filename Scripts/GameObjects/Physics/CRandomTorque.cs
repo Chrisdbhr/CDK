97e69775b630da9720b206ba0a4ee4455b749fc4
Chris <chrisdbhr@gmail.com>
2022-02-06 16:13:59 -0300

    physics scripts

---
+++ Scripts/GameObjects/Physics/CRandomTorque.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace CDK {
+	public class CRandomTorque : CAutoTriggerCompBase {
+		
+		[SerializeField] private Vector3 _torqueRangeMin = new (0f,-1f,0f);
+		[SerializeField] private Vector3 _torqueRangeMax = new (0f,1f,0f);
+		[SerializeField] private Rigidbody _rb;
+		
+		
+		
+		
+		protected override void TriggerEvent() {
+			if (_rb == null) return;
+			_rb.AddTorque(
+				UnityEngine.Random.Range(this._torqueRangeMin.x, this._torqueRangeMax.x),
+				UnityEngine.Random.Range(this._torqueRangeMin.y, this._torqueRangeMax.y),
+				UnityEngine.Random.Range(this._torqueRangeMin.z, this._torqueRangeMax.z)
+			);
+		}
+
+		private void Reset() {
+			if(_rb == null) _rb = this.GetComponent<Rigidbody>();
+		}
+	}
+}
