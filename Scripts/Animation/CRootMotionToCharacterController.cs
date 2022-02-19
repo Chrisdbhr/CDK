c7b10ece23f981d2f25e9feb65eaf02ce4f2d56d
Chris <chrisdbhr@gmail.com>
2022-02-19 02:13:59 -0300

    CRootMotionToCharacterController transfer

---
+++ Scripts/Animation/CRootMotionToCharacterController.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+
+namespace CDK {
+	[RequireComponent(typeof(Animator))]
+	public class CRootMotionToCharacterController : MonoBehaviour {
+
+		private Animator _targetAnimator;
+		public event Action<Vector3> RootMotionDeltaPositionChanged {
+			add {
+				this._rootMotionDeltaPositionChanged -= value;
+				this._rootMotionDeltaPositionChanged += value;
+			}
+			remove {
+				this._rootMotionDeltaPositionChanged -= value;
+			}
+		}
+
+		private Action<Vector3> _rootMotionDeltaPositionChanged;
+
+		
+		
+		
+		private void Awake() {
+			_targetAnimator = this.GetComponent<Animator>();
+		}
+
+		private void OnDestroy() {
+			this._rootMotionDeltaPositionChanged = null;
+		}
+
+		private void OnAnimatorMove() {
+			if (_rootMotionDeltaPositionChanged == null) return;
+			if (CTime.TimeScale == 0f) {
+				_rootMotionDeltaPositionChanged(Vector3.zero);
+				return;
+			}
+
+			if (this._targetAnimator == null) return;
+
+			_rootMotionDeltaPositionChanged(this._targetAnimator.deltaPosition);
+		}
+		
+	}
+}
