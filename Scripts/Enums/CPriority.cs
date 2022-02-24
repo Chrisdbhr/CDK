c82b1c869f9605ca040e111f17b1dc856f193ee3
Chris <chrisdbhr@gmail.com>
2022-02-24 11:42:29 -0300

    CPriority

---
+++ Scripts/Enums/CPriority.cs
@@ -0,0 +1,20 @@
+namespace CDK {
+	public enum CPriority {
+		/// <summary>
+		///   <para>Lowest priority.</para>
+		/// </summary>
+		Low = 0,
+		/// <summary>
+		///   <para>Below normal priority.</para>
+		/// </summary>
+		BelowNormal = 1,
+		/// <summary>
+		///   <para>Normal priority.</para>
+		/// </summary>
+		Normal = 2,
+		/// <summary>
+		///   <para>Highest priority.</para>
+		/// </summary>
+		High = 4,
+	}
+}
