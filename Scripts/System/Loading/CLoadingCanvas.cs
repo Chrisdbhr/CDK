ea82ad49bb21975060d9da9124165a591125e281
Chris <chrisdbhr@gmail.com>
2022-04-15 01:02:35 -0300

    improved loading canvas

--- Scripts/System/Loading/CLoadingCanvas.cs
+++ Scripts/System/Loading/CLoadingCanvas.cs
@@ -1,22 +1,50 @@
 using System;
+using UniRx;
 using UnityEngine;
 
 namespace CDK {
 	public class CLoadingCanvas : MonoBehaviour {
 
-		[SerializeField] private CanvasGroup _loadingUI;
+        #region <<---------- Properties and Fields ---------->>
+        
+        [SerializeField] private Canvas _loadingUI;
+        private IDisposable _timerDisposable;
+        
+        #endregion <<---------- Properties and Fields ---------->>
 
-		private void Awake() {
-			DontDestroyOnLoad(this.gameObject);
-			HideLoadingUI();
-		}
 
-		public void ShowLoadingUI() {
-			_loadingUI.alpha = 1f;
-		}
+
+
+        #region <<---------- MonoBehaviour ---------->>
+        
+        private void Awake() {
+            DontDestroyOnLoad(this.gameObject);
+            HideLoadingUI();
+        }
+
+        private void OnDestroy() {
+            this._timerDisposable?.Dispose();
+        }
+        #endregion <<---------- MonoBehaviour ---------->>
+
+
+
+
+        #region <<---------- General ---------->>
+        
+        public void ShowLoadingUI() {
+            this._timerDisposable?.Dispose();
+            this._timerDisposable = Observable.Timer(TimeSpan.FromSeconds(1f), TimeSpan.Zero).Subscribe(_ => {
+                if (this == null) return;
+                this._loadingUI.enabled = true;
+            });
+        }
 		
-		public void HideLoadingUI() {
-			_loadingUI.alpha = 0f;
-		}
+        public void HideLoadingUI() {
+            this._timerDisposable?.Dispose();
+            this._loadingUI.enabled = false;
+        }
+        
+        #endregion <<---------- General ---------->>
 	}
 }
