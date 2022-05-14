4e6d2ca1cb52670f57339837da29c3be15002829
Chris <chrisdbhr@gmail.com>
2022-05-14 20:07:48 -0300

    fixed unity object null check extension

--- Scripts/Extensions/CUnityObjectExtensions.cs
+++ Scripts/Extensions/CUnityObjectExtensions.cs
@@ -4,6 +4,12 @@ using Object = UnityEngine.Object;
 
 namespace CDK {
     public static class CUnityObjectExtensions {
+        
+        public static void CDoIfNotNull<T>(this T value, Action<T> actionToDo) where T : Object {
+            if (value == null) return;
+            actionToDo?.Invoke(value);
+        }
+        
         public static void CDestroy(this Object value, float time = 0f) {
             if (value == null) return;
             if (Application.isPlaying) {
