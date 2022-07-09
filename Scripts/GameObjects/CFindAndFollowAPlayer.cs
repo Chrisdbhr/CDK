42a4d4473c95333fc124c591c9bd01e5a27938ec
Chris <chrisdbhr@gmail.com>
2022-07-09 00:42:25 -0300

    find and follow player

---
+++ Scripts/GameObjects/CFindAndFollowAPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace CDK {
+    /// <summary>
+    /// Find and follow a Player by Tag.
+    /// </summary>
+    public class CFindAndFollowAPlayer : CTransformFollower {
+        [SerializeField] [TagSelector] private string _playerTag = "Player";
+
+        protected override void Execute(float deltaTime) {
+            if (this.TransformToFollow == null) {
+                GameObject.FindWithTag(this._playerTag)
+                .CDoIfNotNull(g => this.TransformToFollow = g.transform);
+            }
+            base.Execute(deltaTime);
+        }
+    }
+}
