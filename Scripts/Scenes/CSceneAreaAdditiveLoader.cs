c193c7bda1ba2e7fa7d1bb83e275009a385d8e58
Chris <chrisdbhr@gmail.com>
2022-07-24 12:34:40 -0300

    improved scene area additive loader

--- Scripts/Scenes/CSceneAreaAdditiveLoader.cs
+++ Scripts/Scenes/CSceneAreaAdditiveLoader.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Linq;
-using UniRx;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using AsyncOperation = UnityEngine.AsyncOperation;
@@ -36,7 +35,7 @@ namespace CDK {
 		private AsyncOperation _unloadAsyncOperation;
 
 		[NonSerialized] private bool _editorSceneIsDirty;
-		private IDisposable _checkDisposable;
+		private int _updateFrameSkips = 5;
 		
 		#endregion <<---------- Properties and Fields ---------->>
 
@@ -57,14 +56,16 @@ namespace CDK {
 		}
 
 		private void OnEnable() {
-			_checkDisposable?.Dispose();
-			_checkDisposable = Observable.Timer(TimeSpan.FromSeconds(0.1f),TimeSpan.FromSeconds(0.5f)).Subscribe(_ => {
-				this.CheckForObject();
-			});
-			
 			#if UNITY_EDITOR
 			EditorApplication.playModeStateChanged += EditorApplicationOnPlayModeStateChanged;
 			#endif
+		}
+
+		private void Update() {
+			#if !UNITY_EDITOR
+			if (Time.frameCount % this._updateFrameSkips != 0) return;
+			#endif
+			this.CheckForObject();
 		}
 
 		#if UNITY_EDITOR
@@ -74,16 +75,11 @@ namespace CDK {
 		#endif
 
 		private void OnDisable() {
-			_checkDisposable?.Dispose();
 			this._anyTriggerObjectInside = false;
 			
 			#if UNITY_EDITOR
 			EditorApplication.playModeStateChanged -= EditorApplicationOnPlayModeStateChanged;
 			#endif
-		}
-		
-		private void OnDestroy() {
-			_checkDisposable?.Dispose();
 		}
 
 		#if UNITY_EDITOR
@@ -92,12 +88,19 @@ namespace CDK {
 			var tPos = t.position;
 			
 			// color
-			if(this._editorSceneIsDirty) Gizmos.color = Color.yellow;
-			else if(this._anyTriggerObjectInside) Gizmos.color = Color.blue;
-			else Gizmos.color = Color.green;
-			
+			Gizmos.color = new Color(0f,200f,0f, 0.1f);
 			Handles.Label(tPos, t.name);
-			Gizmos.DrawWireCube(tPos, t.localScale);
+			if (this._editorSceneIsDirty) {
+				Gizmos.color = Color.yellow;
+			}
+			
+			if (this._anyTriggerObjectInside) {
+				Gizmos.DrawWireCube(tPos, t.localScale);
+			}
+			else {
+				Gizmos.DrawCube(tPos, t.localScale);
+			}
+			
 		}
 		#endif
 		
@@ -109,7 +112,6 @@ namespace CDK {
 		#region <<---------- General ---------->>
 		
 		void CheckForObject() {
-			if (this == null) return;
 			var t = this.transform;
 				
 			#if UNITY_EDITOR
@@ -120,17 +122,16 @@ namespace CDK {
 				return;
 			}
 			#endif
-				
+
 			var tRotation = t.rotation;
 			
-			
-			 var colliders = Physics.OverlapBox(
-				t.position, 
-				t.localScale*0.5f,
-				tRotation, 
-				this._triggerLayer);
+			var colliders = Physics.OverlapBox(
+			t.position, 
+			t.localScale*0.5f,
+			tRotation, 
+			this._triggerLayer);
 
-			 this.AnyTriggerObjectInside = colliders.Any(c => c.CompareTag(_tag));
+			this.AnyTriggerObjectInside = colliders.Any(c => c.CompareTag(_tag));
 		}
 
 		void OnObjectInsideChanged(bool isInside) {
