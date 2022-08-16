e93c0c7da12559c1acbe92c48ae725956a21563c
Chris <chrisdbhr@gmail.com>
2022-08-16 00:29:34 -0300

    CFixedWorldRotation

---
+++ Scripts/GameObjects/Rotation/CFixedWorldRotation.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace CDK {
+    public class CFixedWorldRotation : CMonoBehaviourUpdateExecutionLoopTime {
+
+        [SerializeField] private Vector3 _axis = new Vector3(90f,0f,0f);
+        
+        
+        protected override void Execute(float deltaTime) {
+            this.transform.rotation = Quaternion.Euler(this._axis);
+        }
+    }
+}
