7dd811c2da9b021559c4860d3adf019f2c9eb843
Chris <chrisdbhr@gmail.com>
2022-08-17 00:00:12 -0300

    CRandomRotation

---
+++ Scripts/GameObjects/Rotation/CRandomRotation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace CDK {
+    public class CRandomRotation : CAutoTriggerCompBase {
+
+        [SerializeField] private Vector3 _rotationMin;
+        [SerializeField] private Vector3 _rotationMax = Vector3.one * 360f;
+        [SerializeField] private Space _relativeTo;
+    
+        
+        
+        protected override void TriggerEvent() {
+            this.transform.Rotate(
+                Random.Range(this._rotationMin.x, this._rotationMax.x),
+                Random.Range(this._rotationMin.y, this._rotationMax.y),
+                Random.Range(this._rotationMin.z, this._rotationMax.z),
+                this._relativeTo
+            );
+        }
+    }
+}
