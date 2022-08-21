9bb6d0a6dfaa06fd2be926c6836122f61b1c4205
Chris <chrisdbhr@gmail.com>
2022-08-21 14:49:02 -0300

    CSetLayerOnAwake

---
+++ Scripts/Graphics/CSetLayerOnAwake.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace CDK {
+    public class CSetLayerOnAwake : MonoBehaviour {
+
+        [SerializeField] private LayerMask _layer = -1;
+        
+        
+        public void Awake() {
+            this.gameObject.CSetLayerFromLayerMask(this._layer);
+            Debug.Log($"Setting '{this.name}' to '{this.gameObject.layer.ToString()}'");
+        }
+    }
+}
