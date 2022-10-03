5e4949f420da39d07a6378c1fa57750a15b9b767
Chris <chrisdbhr@gmail.com>
2022-10-03 01:29:17 -0300

    random trigger improved

--- Scripts/GameObjects/Triggers/CRandomTrigger.cs
+++ Scripts/GameObjects/Triggers/CRandomTrigger.cs
@@ -4,28 +4,34 @@ using UnityEngine.Events;
 namespace CDK {
 	public class CRandomTrigger : CAutoTriggerCompBase {
 
+        #region <<---------- Properties and Fields ---------->>
+
         [Tooltip("Chance in % to trigger event")]
         public float ChanceToTrigger => this._chanceToTrigger;
-		[SerializeField] [Range(1f, 99f)]private float _chanceToTrigger = 50f;
-		[SerializeField] private bool _triggerAffectsSelfActiveState = true;
-
+        [SerializeField] [Range(1f, 99f)]private float _chanceToTrigger = 50f;
+        [SerializeField] protected bool _triggerValue;
+        [SerializeField] private bool _triggerAffectsSelfActiveState = true;
         [SerializeField] private CUnityEventBool _randomResultEvent;
         [SerializeField] private CUnityEventBool _randomResultInvertedEvent;
-		[SerializeField] private UnityEvent _randomPositiveEvent;
-		[SerializeField] private UnityEvent _randomNegativeEvent;
+        [SerializeField] private UnityEvent _randomPositiveEvent;
+        [SerializeField] private UnityEvent _randomNegativeEvent;
+
+        #endregion <<---------- Properties and Fields ---------->>
+        
+        
 		
 		
 		protected override void TriggerEvent() {
             var chance = GetChance();
-			bool willTrigger = (chance <= this._chanceToTrigger);
+            this._triggerValue = (chance <= this._chanceToTrigger);
 
 			if (this._triggerAffectsSelfActiveState) {
-				this.gameObject.SetActive(willTrigger);
+				this.gameObject.SetActive(this._triggerValue);
 			}
 			
-            this._randomResultEvent?.Invoke(willTrigger);
-            this._randomResultInvertedEvent?.Invoke(!willTrigger);
-			if (willTrigger) {
+            this._randomResultEvent?.Invoke(this._triggerValue);
+            this._randomResultInvertedEvent?.Invoke(!this._triggerValue);
+			if (this._triggerValue) {
 				this._randomPositiveEvent?.Invoke();
 			}
 			else {
