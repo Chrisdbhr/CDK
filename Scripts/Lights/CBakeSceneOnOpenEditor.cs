fd3a0b63c5bedf77e8b955eb0f3e496c880ee452
Chris <chrisdbhr@gmail.com>
2022-10-07 00:07:29 -0300

    CBakeSceneOnOpenEditor

---
+++ Scripts/Lights/CBakeSceneOnOpenEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace CDK {
+    [ExecuteInEditMode]
+    public class CBakeSceneOnOpenEditor : MonoBehaviour {
+        private void Awake() {
+            if (Application.isPlaying) return;
+            #if UNITY_EDITOR
+            Debug.Log("Auto baking scene.", this);
+            Lightmapping.BakeAsync();
+            #endif
+        }
+    }
+}
