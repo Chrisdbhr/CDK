eb89aecf12a1a4cf9ac274976166afa159a10e60
Chris <chrisdbhr@gmail.com>
2022-11-09 18:35:39 -0300

    Input handling improved

--- Scripts/Input/CInputManager.cs
+++ Scripts/Input/CInputManager.cs
@@ -9,23 +9,32 @@ using Rewired;
 namespace CDK {
 	public static class CInputManager {
 
-		public enum InputType { // beware when changing name because there is compile conditions using this values
-			MouseAndKeyboard,
-			JoystickController,
-			Mobile
-		}
+        #region <<---------- Enums ---------->>
+
+        public enum InputType { // beware when changing name because there is compile conditions using this values
+            Keyboard,
+            Mouse,
+            JoystickController,
+            Mobile
+        }
+
+        #endregion <<---------- Enums ---------->>
+
+        
+        
+        
+        #region <<---------- Properties and Fields ---------->>
 
 		public static InputType ActiveInputType {
 			get => _activeInputType;
 			private set {
 				if (value == _activeInputType) return;
-				Debug.Log($"Input type changed to '{value.ToString()}'");
+                if(!value.IsMouseOrKeyboard() && !_activeInputType.IsMouseOrKeyboard()) Debug.Log($"Input type changed to '{value.ToString()}'");
 				_activeInputType = value;
 				_inputTypeChanged?.Invoke(value);
 			}
 		}
 		private static InputType _activeInputType;
-
 
 		public static event Action<InputType> InputTypeChanged {
 			add {
@@ -35,9 +44,11 @@ namespace CDK {
 			remove => _inputTypeChanged -= value;
 		}
 		private static Action<InputType> _inputTypeChanged;
+        
+        #endregion <<---------- Properties and Fields ---------->>
 
 
-
+        
 
 		/// <summary>
 		/// ANTES da scene load.
@@ -61,26 +72,45 @@ namespace CDK {
 
 			// wait one frame
 			Observable.EveryUpdate().Subscribe(_ => {
-				if (ReInput.controllers.GetAnyButtonChanged(ControllerType.Mouse) || ReInput.controllers.GetAnyButton(ControllerType.Keyboard)) {
-					ActiveInputType = InputType.MouseAndKeyboard;
-				}else if (ReInput.controllers.GetAnyButtonChanged(ControllerType.Joystick) || ReInput.controllers.GetAnyButton(ControllerType.Custom)) {
+                if (CApplication.IsQuitting || ReInput.controllers == null) return;
+                
+				if (ReInput.controllers.GetAnyButton(ControllerType.Keyboard)) {
+                    ActiveInputType = InputType.Keyboard;
+                    return;
+                }
+                
+                if (ReInput.controllers.GetAnyButtonChanged(ControllerType.Mouse) || ReInput.controllers.Mouse.screenPositionDelta != default) {
+                    ActiveInputType = InputType.Mouse;
+                    return;
+                }
+                
+                if (ReInput.controllers.GetAnyButtonChanged(ControllerType.Joystick) || ReInput.controllers.GetAnyButton(ControllerType.Custom)) {
 					ActiveInputType = InputType.JoystickController;
-				}
+                    return;
+                }
+                
 				// TODO check mobile input
+                
 			});
 		}
 		#endif
 
 		private static void SetControllerTypeBasedOnPlatform() {
 			#if UNITY_STANDALONE || UNITY_WEBGL
-			ActiveInputType = InputType.MouseAndKeyboard;
+			ActiveInputType = InputType.Keyboard;
 			#elif UNITY_ANDROID || UNITY_IOS || UNITY_IPHONE
 			ActiveInputType = InputType.Mobile;
 			#else
 			ActiveInputType = InputType.JoystickController;
 			#endif
-			Debug.Log($"Active input type set to: {ActiveInputType.ToString()}");
+			Debug.Log($"{nameof(SetControllerTypeBasedOnPlatform)} input type auto set to: {ActiveInputType.ToString()}");
 		}
 		
 	}
+    
+    public static class InputTypeExtension {
+        public static bool IsMouseOrKeyboard(this CInputManager.InputType input) {
+            return (input == CInputManager.InputType.Keyboard || input == CInputManager.InputType.Mouse);
+        }
+    }
 }
