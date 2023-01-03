70e06c499f84a73840d5cc763dff3cb29c23832a
Chris <chrisdbhr@gmail.com>
2023-01-03 05:20:34 -0300

    music for scene pause

--- Scripts/Audio/CMusicForScene.cs
+++ Scripts/Audio/CMusicForScene.cs
@@ -1,8 +1,6 @@
 using System;
-using UniRx;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
-using Object = UnityEngine.Object;
 
 #if FMOD
 using FMODUnity;
@@ -18,16 +16,10 @@ namespace CDK.Audio {
 		[NonSerialized] private FMOD.Studio.EventInstance _musicState;
 		#endif
 
-        private CRetainable PauseMusicRetainable;
 
-
-
-
+        
 
 		#region <<---------- MonoBehaviour ---------->>
-        private void Awake() {
-            PauseMusicRetainable = new CRetainable();
-        }
 
         private void OnEnable() {
 			#if FMOD
@@ -35,19 +27,8 @@ namespace CDK.Audio {
 				Debug.LogWarning($"{this.name} is not referencing a valid Music (EventRef)!");
 				return;
 			}
-				
-			this._musicState = RuntimeManager.CreateInstance(this._music);
-
-			if (this._is3d) {
-				this._musicState.set3DAttributes(this.transform.position.To3DAttributes());
-			}
-			
-			if(this._musicState.isValid()) this._musicState.start();
+            this.Play();
 			#endif
-
-            PauseMusicRetainable.IsRetainedRx.DistinctUntilChanged().TakeUntilDisable(this).Subscribe(pause => {
-                if (this._musicState.isValid()) this._musicState.setPaused(pause);
-            });
         }
 		
 		private void OnDisable() {
@@ -63,9 +44,22 @@ namespace CDK.Audio {
 		#endregion <<---------- MonoBehaviour ---------->>
 
 
+        
 
 		#region <<---------- Music ---------->>
 
+        public void Play() {
+            #if FMOD
+            this._musicState = RuntimeManager.CreateInstance(this._music);
+
+            if (this._is3d) {
+                this._musicState.set3DAttributes(this.transform.position.To3DAttributes());
+            }
+			
+            if(this._musicState.isValid()) this._musicState.start();
+            #endif
+        }
+        
         public void Stop(bool allowFadeout) {
 			#if FMOD
 			this._musicState.stop(allowFadeout ? STOP_MODE.ALLOWFADEOUT : STOP_MODE.IMMEDIATE);
@@ -73,17 +67,6 @@ namespace CDK.Audio {
 			throw new NotImplementedException();
 			#endif
 		}
-
-        public void PauseMusic(Object requester) {
-            PauseMusicRetainable.Retain(requester);
-        }
-
-        public void UnPauseMusic(Object requester) {
-            if (requester == null || this.enabled == false) {
-                this.Stop(false);
-            }
-            PauseMusicRetainable.Release(requester);
-        }
 		
 		#endregion <<---------- Music ---------->>
 	}
