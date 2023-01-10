64639fdbbc7ebdd2ff1ab9a35ef7869ded9afeec
Chris <chrisdbhr@gmail.com>
2023-01-10 17:06:27 -0300

    CDestroyOnContact

---
+++ Scripts/GameObjects/Physics/CDestroyOnContact.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace CDK {
+    public class CDestroyOnContact : CPhysicsTrigger {
+
+        [SerializeField] private GameObject[] _otherGameObjectsToDestroy;
+        
+        
+        protected override void StartedCollisionOrTrigger(Transform other) {
+            base.StartedCollisionOrTrigger(other);
+            foreach (var o in _otherGameObjectsToDestroy) {
+                o.CDestroy();
+            }
+            this.gameObject.CDestroy();
+        }
+    }
+}
