d38d662bb4239d3f576712452f41da7aa9473230
Chris <chrisdbhr@gmail.com>
2023-01-25 02:37:08 -0300

    physics

---
+++ Scripts/Physics/CPhysics.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace CDK {
+    public abstract class CPhysics {
+        
+        public const float DefaultGravityY = -9.81f;
+        
+        public static float GetDistanceFromDefaultGravity() {
+            return (DefaultGravityY - Physics.gravity.y).CAbs();
+        }
+
+    }
+}
