28537cf6538eb338ce0f6920ed310cb863c40f88
Chris <chrisdbhr@gmail.com>
2023-02-02 18:47:24 -0300

    CPlayerPlatformTrigger improved

--- Scripts/System/Platform/CPlayerPlatformTrigger.cs
+++ Scripts/System/Platform/CPlayerPlatformTrigger.cs
@@ -1,21 +1,31 @@
 ﻿using UnityEngine;
-using UnityEngine.Events;
 
 namespace CDK {
     public class CPlayerPlatformTrigger : CAutoTriggerCompBase {
 
-        [SerializeField] private UnityEvent _isWebgl;
+        [SerializeField] private CUnityEventBool _isWebgl;
+        [SerializeField] private CUnityEventBool _isMobile;
+        [SerializeField] private CUnityEventBool _isConsole;
         
         
         
         
         protected override void TriggerEvent() {
-            switch (Application.platform) {
-                case RuntimePlatform.WebGLPlayer: {
-                    this._isWebgl?.Invoke();
-                    break;
-                }
-            }
+            this._isWebgl?.Invoke(Application.platform == RuntimePlatform.WebGLPlayer);
+            this._isMobile?.Invoke(IsMobilePlatform());
+            this._isConsole?.Invoke(
+                Application.platform == RuntimePlatform.XboxOne 
+                || Application.platform == RuntimePlatform.PS4
+                || Application.platform == RuntimePlatform.PS5
+                || Application.platform == RuntimePlatform.Switch
+                || Application.platform == RuntimePlatform.GameCoreXboxSeries
+                || Application.platform == RuntimePlatform.GameCoreXboxOne
+            );
         }
+
+        public static bool IsMobilePlatform() {
+            return Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer;
+        }
+        
     }
 }
