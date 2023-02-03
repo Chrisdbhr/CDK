4caf6c8332806b6ca04cf7c296c5fea54d60575d
Chris <chrisdbhr@gmail.com>
2023-02-03 11:22:58 -0300

    is console trigger

--- Scripts/System/Platform/CPlayerPlatformTrigger.cs
+++ Scripts/System/Platform/CPlayerPlatformTrigger.cs
@@ -13,18 +13,20 @@ namespace CDK {
         protected override void TriggerEvent() {
             this._isWebgl?.Invoke(Application.platform == RuntimePlatform.WebGLPlayer);
             this._isMobile?.Invoke(IsMobilePlatform());
-            this._isConsole?.Invoke(
-                Application.platform == RuntimePlatform.XboxOne 
-                || Application.platform == RuntimePlatform.PS4
-                || Application.platform == RuntimePlatform.PS5
-                || Application.platform == RuntimePlatform.Switch
-                || Application.platform == RuntimePlatform.GameCoreXboxSeries
-                || Application.platform == RuntimePlatform.GameCoreXboxOne
-            );
+            this._isConsole?.Invoke(IsConsolePlatform());
         }
 
         public static bool IsMobilePlatform() {
             return Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer;
+        }
+        
+        public static bool IsConsolePlatform() {
+            return Application.platform == RuntimePlatform.XboxOne 
+                || Application.platform == RuntimePlatform.PS4
+                || Application.platform == RuntimePlatform.PS5
+                || Application.platform == RuntimePlatform.Switch
+                || Application.platform == RuntimePlatform.GameCoreXboxSeries
+                || Application.platform == RuntimePlatform.GameCoreXboxOne;
         }
         
     }
