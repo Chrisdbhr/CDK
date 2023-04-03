72a9f4e35ca541e6848f6dfc394af1d82a793fcb
Chris <chrisdbhr@gmail.com>
2023-04-03 10:20:49 -0300

    color extensions

---
+++ Scripts/Extensions/CColorExtensions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace CDK {
+    public static class CColorExtensions {
+
+        public static Color CRandomColor() {
+            // Generate random RGB values between 0 and 1.
+            float r = Random.Range(0f, 1f);
+            float g = Random.Range(0f, 1f);
+            float b = Random.Range(0f, 1f);
+
+            // Create and return a new Color object using the random RGB values.
+            return new Color(r, g, b);
+        }
+        
+    }
+}
