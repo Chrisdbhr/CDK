a2125c86b8b372890c3df0dc5c22deba776c4b2c
Chris <chrisdbhr@gmail.com>
2023-05-17 23:30:47 -0300

    C Scene Field improved

--- Scripts/System/CSceneField.cs
+++ Scripts/System/CSceneField.cs
@@ -1,39 +1,77 @@
 using System;
+using System.IO;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace CDK {
 	[System.Serializable]
 	public class CSceneField : ISerializationCallbackReceiver {
+        
+        #region <<---------- Properties and Fields ---------->>
+
 		#if UNITY_EDITOR
-		[Obsolete("Do not use on runtime scripts! This is a editor only variable.")]
-		public UnityEditor.SceneAsset sceneAsset;
+        [Obsolete("Do not use on runtime scripts! This is a editor only variable.")]
+        public SceneAsset sceneAsset;
 		#endif
 
 #pragma warning disable 414
-		[SerializeField, HideInInspector]
-		private string sceneName = "";
+        [SerializeField, HideInInspector]
+        private string sceneName = "";
 #pragma warning restore 414
 
-		// Makes it work with the existing Unity methods (LoadLevel/LoadScene)
-		public static implicit operator string(CSceneField sceneField) {
+        #endregion <<---------- Properties and Fields ---------->>
+
+        
+        
+
+        #region <<---------- Operator Overload ---------->>
+
+        // Makes it work with the existing Unity methods (LoadLevel/LoadScene)
+        public static implicit operator string(CSceneField sceneField) {
 			#if UNITY_EDITOR
-			return System.IO.Path.GetFileNameWithoutExtension(UnityEditor.AssetDatabase.GetAssetPath(sceneField.sceneAsset));
+            return Path.GetFileNameWithoutExtension(AssetDatabase.GetAssetPath(sceneField.sceneAsset));
 			#else
 			return sceneField.sceneName;
 			#endif
-		}
+        }
         
         public static bool operator ==(CSceneField a, CSceneField b) {
             return a?.sceneName == b?.sceneName;
         }
         public static bool operator !=(CSceneField a, CSceneField b) => !(a == b);
 
-		public void OnBeforeSerialize() {
-			#if UNITY_EDITOR
-			this.sceneName = this;
-			#endif
-		}
+        #endregion <<---------- Operator Overload ---------->>
 
-		public void OnAfterDeserialize() { }
+
+        
+        
+        #region <<---------- Serialization ---------->>
+
+        public void OnBeforeSerialize() {
+			#if UNITY_EDITOR
+            this.sceneName = this;
+			#endif
+        }
+
+        public void OnAfterDeserialize() { }
+
+        #endregion <<---------- Serialization ---------->>
+
+
+        
+
+        #region <<---------- Public Methods ---------->>
+        
+        public void LoadScene() {
+            SceneManager.LoadScene(this.sceneName);
+        }
+
+        public void LoadSceneAdditive() {
+            SceneManager.LoadScene(this.sceneName, LoadSceneMode.Additive);
+        }
+
+        #endregion <<---------- Public Methods ---------->>
+
 	}
 }
