b2b25fb90e62e866111d644c44d89e584d9be3f4
Chris <chrisdbhr@gmail.com>
2023-06-20 02:28:15 -0300

    cthread wip

---
+++ Scripts/System/CThread.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using UnityEngine;
+
+namespace CDK {
+    public static class CThread {
+
+        public static Thread MainThread { get; private set; } 
+        
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
+        private static void InitializeBeforeSplashScreen() {
+            MainThread = Thread.CurrentThread;
+        }
+        
+    }
+}
