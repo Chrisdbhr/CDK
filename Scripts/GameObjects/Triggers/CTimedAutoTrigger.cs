599ea5761bac90c1532846a8dc4270226efb2b62
Chris <chrisdbhr@gmail.com>
2023-09-15 13:58:38 -0300

    CTimedAutoTrigger

---
+++ Scripts/GameObjects/Triggers/CTimedAutoTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace CDK {
+    public class CTimedAutoTrigger : CAutoTriggerCompBase {
+
+        [SerializeField, Min(0f)] private float _secondsToTrigger = 5f;
+        [SerializeField] private UnityEvent _onTriggerEvent;
+
+
+
+
+        protected override void TriggerEvent() {
+            this.CStartCoroutine(this.TriggerRoutine());
+        }
+
+        private IEnumerator TriggerRoutine() {
+            yield return new WaitForSeconds(this._secondsToTrigger);
+            this._onTriggerEvent?.Invoke();
+        }
+
+    }
+}
