fa93048c8b966dd709ce09cd7a956d7499343900
Chris <chrisdbhr@gmail.com>
2023-09-15 13:57:18 -0300

    COpenManifestShortcut

---
+++ Scripts/Editor/Utility/COpenManifestShortcut.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+
+namespace CDK.Editor {
+    /// <summary>
+    /// Adds a menu item that opens the Package Manifest json, bellow the Package Manager button.
+    /// </summary>
+    public class COpenManifestShortcut {
+
+        [MenuItem("Window/Package Manifest", priority = 1501)]
+        static void OpenPackageManifest() {
+            EditorUtility.OpenWithDefaultApp("Packages/manifest.json");
+        }
+
+    }
+}
