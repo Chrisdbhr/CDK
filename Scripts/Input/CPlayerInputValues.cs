f83eec5406063dd6ccb61dfff3cbddbaa4d33bec
Chris <chrisdbhr@gmail.com>
2023-09-26 19:13:47 -0300

    input values improved

--- Scripts/Input/CPlayerInputValues.cs
+++ Scripts/Input/CPlayerInputValues.cs
@@ -6,16 +6,36 @@ namespace CDK {
         /// <summary>
         /// Can be relative to camera position. 
         /// </summary>
-        public Vector2 Movement2D => new Vector2(Movement3D.x, Movement3D.z);
+        public Vector2 RelativeMovement2D => new Vector2(this.RelativeMovement3D.x, this.RelativeMovement3D.z);
         /// <summary>
         /// Can be relative to camera position. 
         /// </summary>
-        public Vector3 Movement3D;
-        public float MovementSqrMagnitude => this.Movement3D.sqrMagnitude;
+        public Vector3 RelativeMovement3D { get; private set; }
+        /// <summary>
+        /// Movement not relative to camera position.
+        /// </summary>
+        public Vector3 RawMovement3D { get; private set; }
+        public float MovementSqrMagnitude => this.RelativeMovement3D.sqrMagnitude;
         public bool IsDoingMovementInput => MovementSqrMagnitude > 0.01f;
         
-        public bool    Walk;
-        public bool    Run;
-        public bool    Aim;
+        public bool Walk;
+        public bool Run;
+        public bool Aim;
+
+        public void SetRawMovement3D(float x, float z) {
+            var value = this.RawMovement3D;
+            value.x = x.CClamp(-1f, 1f);
+            value.y = 0f;
+            value.z = z.CClamp(-1f, 1f);
+            this.RawMovement3D = value;
+        }
+
+        public void SetMovement3DRelativeToCamera(Transform cameraTransform) {
+            var value = cameraTransform.rotation * this.RawMovement3D;
+            value.x = value.x.CClamp(-1f, 1f);
+            value.y = 0f;
+            value.z = value.z.CClamp(-1f, 1f);
+            this.RelativeMovement3D = value;
+        }
     }
 }
