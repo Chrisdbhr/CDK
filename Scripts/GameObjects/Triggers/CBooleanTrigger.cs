d09f56a0dcda329f848832834ccce2c145c884b8
Chris <chrisdbhr@gmail.com>
2023-09-30 02:20:44 -0300

    boolean trigger

---
+++ Scripts/GameObjects/Triggers/CBooleanTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace CDK {
+    public class CBooleanTrigger : MonoBehaviour {
+
+        public bool Value;
+        [SerializeField] private UnityEvent _onValueTrue;
+        [SerializeField] private UnityEvent _onValueFalse;
+        [SerializeField] private CUnityEventBool _onAnyValue;
+
+
+
+
+        public void TriggerValue(bool value) {
+            this.Value = value;
+            if(value) this._onValueTrue?.Invoke();
+            else this._onValueFalse?.Invoke();
+            this._onAnyValue?.Invoke(value);
+        }
+
+    }
+}
