4bb760402ed4bb69d1ba4b5338b3ac47c8e5e09b
Chris <chrisdbhr@gmail.com>
2023-10-19 04:09:19 -0300

    CAppInitializeBeforeTheSplashTrigger

--- Scripts/System/CAppInitializeBeforeTheSplashTrigger.cs
+++ Scripts/System/CAppInitializeBeforeTheSplashTrigger.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections;
-using UniRx;
+﻿using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using Random = UnityEngine.Random;
 
 #if FMOD
 using FMODUnity;
@@ -20,71 +18,61 @@ using UnityEngine.Localization.Settings;
 namespace CDK {
     public class CAppInitializeBeforeTheSplashTrigger : MonoBehaviour {
 
-        [SerializeField] private CanvasGroup _loadingCanvas;
         [SerializeField] private CSceneField _nextScene;
-        private Coroutine _initializationRoutine;
-        private IDisposable _disposeOnDisable;
+        [SerializeField] private TextMeshProUGUI _textLoadState;
+
 
 
 
         #region <<---------- MonoBehaviour ---------->>
-        
-        private void Awake() {
-            if(this._loadingCanvas) this._loadingCanvas.alpha = 0f;
-            this._initializationRoutine = this.CStartCoroutine(Initialize());
+
+        private IEnumerator Start() {
+            Debug.Log("Initializing scene that preloads initial assets.");
+
+            this.CStartCoroutine(DelayedShowText());
+
+            #if Rewired
+            SetStateText("Waiting for Rewired to get ready...");
+            while (!ReInput.isReady) {
+                yield return null;
+            }
+            #endif
+            #if FMOD
+            SetStateText("Waiting for FMOD to Load Master Banks...");
+            while (!RuntimeManager.HaveMasterBanksLoaded) {
+                yield return null;
+            }
+            #endif
+
+            #if UnityLocalization
+            SetStateText("Waiting for Localization System get ready...");
+            yield return LocalizationSettings.InitializationOperation;
+            #endif
+
+            yield return null;
+
+            SetStateText($"Starting to load next scene: '{this._nextScene}'");
+            var asyncOp = SceneManager.LoadSceneAsync(this._nextScene, LoadSceneMode.Single);
+            asyncOp.allowSceneActivation = true;
         }
 
-        private void OnEnable() {
-            this._disposeOnDisable = Observable.Timer(TimeSpan.FromSeconds(1f))
-            .Subscribe(_ => {
-                if (this._loadingCanvas) {
-                    this._loadingCanvas.alpha = 1f;
-                }
-            });
-        }
-
-        private void OnDisable() {
-            this._disposeOnDisable?.Dispose();
-            this.CStopCoroutine(this._initializationRoutine);
-        }
-        
         #endregion <<---------- MonoBehaviour ---------->>
 
 
 
 
         #region <<---------- General ---------->>
-        
-        private IEnumerator Initialize() {
-            Debug.Log("Initializing scene that preloads initial assets.");
-            
-            #if Rewired
-            Debug.Log("Waiting for Rewired to get ready.");
-            while (!ReInput.isReady) {
-                yield return null;
-            }
-            Debug.Log("Rewired is ready.");
-            #endif
-            #if FMOD
-            Debug.Log("Waiting for FMOD to Load Master Banks.");
-            while (!RuntimeManager.HaveMasterBanksLoaded) {
-                yield return null;
-            }
-            Debug.Log("FMOD Master Banks are loaded.");
-            #endif
 
-            #if UnityLocalization
-            yield return LocalizationSettings.InitializationOperation;
-            #endif
+        private IEnumerator DelayedShowText() {
+            yield return new WaitForSeconds(2f);
+            _textLoadState.enabled = true;
+        }
 
-            yield return null;
-            
-            Debug.Log($"Starting to load next scene: '{this._nextScene}'.");
-            var asyncOp = SceneManager.LoadSceneAsync(this._nextScene, LoadSceneMode.Single);
-            asyncOp.allowSceneActivation = true;
+        void SetStateText(string text) {
+            _textLoadState.text = text;
+            Debug.Log(text);
         }
 
         #endregion <<---------- General ---------->>
-
     }
 }
