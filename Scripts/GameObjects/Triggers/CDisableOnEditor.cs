55239c5ea8f04e3ae9ac308b2ccc482b852c1962
Chris <chrisdbhr@gmail.com>
2023-10-31 06:33:17 -0300

    CDisableOnEditor

---
+++ Scripts/GameObjects/Triggers/CDisableOnEditor.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace CDK {
+    public class CDisableOnEditor : MonoBehaviour {
+        private void Awake() {
+            Debug.Log($"Disabling {this.name} on Editor only.", this);
+            this.gameObject.SetActive(false);
+        }
+    }
+}
