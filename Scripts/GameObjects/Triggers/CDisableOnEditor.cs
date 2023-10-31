1c121b47013efa1c551a807e80e08f3cb4a45734
Chris <chrisdbhr@gmail.com>
2023-10-31 07:29:25 -0300

    fix missing code

--- Scripts/GameObjects/Triggers/CDisableOnEditor.cs
+++ Scripts/GameObjects/Triggers/CDisableOnEditor.cs
@@ -2,9 +2,11 @@
 
 namespace CDK {
     public class CDisableOnEditor : MonoBehaviour {
+        #if UNITY_EDITOR
         private void Awake() {
             Debug.Log($"Disabling {this.name} on Editor only.", this);
             this.gameObject.SetActive(false);
         }
+        #endif
     }
 }
