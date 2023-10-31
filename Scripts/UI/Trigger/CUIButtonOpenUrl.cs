ef8e6f31311d710a621c42581239c327f53ec28c
Chris <chrisdbhr@gmail.com>
2023-10-31 16:39:30 -0300

    open url button

---
+++ Scripts/UI/Trigger/CUIButtonOpenUrl.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace CDK.UI.Trigger {
+    [RequireComponent(typeof(Button))]
+    public class CUIButtonOpenUrl : MonoBehaviour {
+
+        #if UNITY_EDITOR
+        [MenuItem("CONTEXT/Button/Add open url component")]
+        static void EditorAddOpenUrl(MenuCommand command) {
+            var target = command.context as Button;
+            Undo.AddComponent(target.gameObject, typeof(CUIButtonOpenUrl));
+        }
+        #endif
+
+        private Button button;
+        [SerializeField] private string urlToOpen = "https://chrisjogos.com";
+
+
+        private void Awake() {
+            button = this.GetComponent<Button>();
+            button.onClick.AddListener(() => {
+                CApplication.OpenURL(urlToOpen);
+            });
+        }
+
+
+
+    }
+}
