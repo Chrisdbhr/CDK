90cce1a27ea5885dbb1b24a34ddc477957e28669
Chris <chrisdbhr@gmail.com>
2023-11-16 15:40:22 -0300

    CQualitySettings

---
+++ Scripts/System/CQualitySettings.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace CDK {
+    public static class CQualitySettings {
+
+        public static bool GetVsync() {
+            return QualitySettings.vSyncCount == 1;
+        }
+
+        public static void SetVsync(bool value) {
+            QualitySettings.vSyncCount = value ? 1 : 0;
+        }
+
+    }
+}
