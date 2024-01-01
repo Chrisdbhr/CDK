1bb8abe15955ed971bd8a5702c07d53512ffe699
Chris <19819051+Chrisdbhr@users.noreply.github.com>
2024-01-01 12:27:43 -0300

    CSkinnedMeshToMesh

---
+++ Scripts/Editor/Tools/CSkinnedMeshToMesh.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace CDK.Editor {
+    public static class CSkinnedMeshToMesh {
+        [MenuItem("CONTEXT/SkinnedMeshRenderer/Convert to Static Mesh Renderer")]
+        private static void ConvertToStaticMeshRenderer(MenuCommand data) {
+            if (!(data?.context is Component comp) || !(comp is SkinnedMeshRenderer skinnedMesh)) return;
+
+            var go = skinnedMesh.gameObject;
+            if (go == null) return;
+
+            var meshRenderer = Undo.AddComponent<MeshRenderer>(go);
+            meshRenderer.sharedMaterials = skinnedMesh.sharedMaterials;
+            var meshFilter = Undo.AddComponent<MeshFilter>(go);
+            meshFilter.sharedMesh = new Mesh();
+            skinnedMesh.BakeMesh(meshFilter.sharedMesh, true);
+            Undo.DestroyObjectImmediate(skinnedMesh);
+
+            Debug.Log($"Converted {go.name} to static mesh renderer");
+        }
+    }
+}
