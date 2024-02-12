a92ce630c149cd5499db354934ca5d79a5a8cd52
Chris <19819051+Chrisdbhr@users.noreply.github.com>
2024-02-12 16:19:53 -0300

    triggers

--- Scripts/GameObjects/Triggers/CEnableTrigger.cs
+++ Scripts/GameObjects/Triggers/CEnableTrigger.cs
@@ -1,13 +1,24 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
 namespace CDK {
 	public class CEnableTrigger : MonoBehaviour {
+        [SerializeField, Min(0f)] private float _delayInSeconds;
 		[SerializeField] private UnityEvent TriggerEvent;
 		
 		private void OnEnable() {
-			this.TriggerEvent?.Invoke();
+            if (this._delayInSeconds > 0f) {
+                this.CStartCoroutine(this.EnableRoutine());
+                return;
+            }
+            this.TriggerEvent?.Invoke();
 		}
+
+        IEnumerator EnableRoutine() {
+            yield return new WaitForSecondsRealtime(this._delayInSeconds);
+            this.TriggerEvent?.Invoke();
+        }
 		
 	}
 }
--- Scripts/GameObjects/Triggers/CStartTrigger.cs
+++ Scripts/GameObjects/Triggers/CStartTrigger.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
@@ -7,12 +8,22 @@ namespace CDK {
 
         [SerializeField, Min(0f)] private float _delay;
 		[SerializeField] private UnityEvent Event;
-
+        private bool alreadyCalled;
 
         IEnumerator Start() {
             if(_delay > 0f) yield return new WaitForSecondsRealtime(_delay);
             Event?.Invoke();
             yield break;
         }
-	}
+
+        private void OnEnable() {
+            if (this.alreadyCalled && _delay > 0f) {
+                Debug.LogWarning($"This {nameof(CStartTrigger)} has already been called its Start() and has a delay. Its possible that the {this.Event.GetPersistentEventCount()} events have been not triggered.");
+            }
+        }
+
+        private void OnDisable() {
+            alreadyCalled = true;
+        }
+    }
 }
