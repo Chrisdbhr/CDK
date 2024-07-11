23d6b9773b30e4cdf41e46c3febbaa2a006a8e31
Chris <19819051+Chrisdbhr@users.noreply.github.com>
2024-07-11 19:18:27 -0300

    Dependecy Injection first codes and scope

---
+++ Scripts/DI/DIHelper.cs
@@ -0,0 +1,15 @@
+﻿using Reflex.Extensions;
+using UnityEngine.SceneManagement;
+
+namespace CDK.DI {
+    /// <summary>
+    /// Dependency Injection helper.
+    /// </summary>
+    public static class DIHelper {
+
+        public static TContract ResolveFromActiveScene<TContract>() {
+            return SceneManager.GetActiveScene().GetSceneContainer().Resolve<TContract>();
+        }
+        
+    }
+}
