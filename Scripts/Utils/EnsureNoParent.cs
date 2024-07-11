3023402e55e59c06d58d9ecfff9e08c9da6b7bd3
Chris <19819051+Chrisdbhr@users.noreply.github.com>
2024-07-11 19:18:37 -0300

    EnsureNoParent

---
+++ Scripts/Utils/EnsureNoParent.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace CDK {
+    [ExecuteInEditMode, DisallowMultipleComponent]
+    public class EnsureNoParent : MonoBehaviour {
+        void OnValidate() {
+            EnsureNoParentObject();
+        }
+
+        void Reset() {
+            EnsureNoParentObject();
+        }
+
+        void EnsureNoParentObject() {
+            if (transform.parent == null) return;
+            transform.parent = null;
+            Debug.LogWarning($"{name} need to be a root object. It parent has been removed.", this);
+            MarkSceneAsDirty();
+        }
+
+        void MarkSceneAsDirty() {
+            #if UNITY_EDITOR
+            if (Application.isPlaying) return;
+            EditorUtility.SetDirty(gameObject);
+            UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(gameObject.scene);
+            #endif
+        }
+    }
+}
