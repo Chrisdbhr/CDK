a2273a6aa230af0a2594c9f64f9c3387dfcd51d2
Chris <19819051+Chrisdbhr@users.noreply.github.com>
2024-07-18 04:40:54 -0300

    build process method to create shortcut to 'Logs and Save Directory'

--- Scripts/Editor/CBuildProcessMethods.cs
+++ Scripts/Editor/CBuildProcessMethods.cs
@@ -1,14 +1,47 @@
-﻿using UnityEditor.Build;
+﻿using System.Diagnostics;
+using System.IO;
+using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 namespace CDK {
     public class CBuildProcessMethods : IPostprocessBuildWithReport {
 
-        public int callbackOrder => 0;
+        public int callbackOrder => 1;
         
         public void OnPostprocessBuild(BuildReport report) {
-            if (report == null || report.summary.result != BuildResult.Succeeded) return;
+            if (report.summary.result != BuildResult.Succeeded && report.summary.result != BuildResult.Unknown) return;
             CEditorPlayerSettings.RaiseBuildVersion();
+            var outputPath = report.summary.outputPath.Replace($"/{Application.productName}.exe", string.Empty);
+            // Create Shortcut to application Logs Directory
+            CreateShortcut(outputPath);
+        }
+
+        static void CreateShortcut(string outputPath)
+        {
+            string targetPath = Path.Combine("%USERPROFILE%\\AppData\\LocalLow", Application.companyName, Application.productName);
+            string shortcutPath = Path.Combine(outputPath, "Logs and Save Directory.lnk");
+
+            string cmd = $"/c powershell -Command \"$WshShell = New-Object -ComObject WScript.Shell; $Shortcut = $WshShell.CreateShortcut('{shortcutPath}'); $Shortcut.TargetPath = '{targetPath}'; $Shortcut.Save()\"";
+
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                Arguments = cmd,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using (Process process = new Process())
+            {
+                process.StartInfo = startInfo;
+                process.Start();
+                string output = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+                UnityEngine.Debug.Log($"Shortcut creation output: {output}");
+            }
         }
 
     }
