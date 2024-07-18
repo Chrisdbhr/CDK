4d893e9f1ab18ac01c8f52b9eea254ca29801cd8
Chris <19819051+Chrisdbhr@users.noreply.github.com>
2024-07-18 04:39:16 -0300

    CStrings

--- Scripts/CStrings.cs
+++ Scripts/CStrings.cs
@@ -1,8 +1,9 @@
-﻿namespace CDK {
-    public static class CDKStrings {
-        
-        public const string PrefixScripts = "CDK/";
-        public const string DontDestroyOnLoad = "DontDestroyOnLoad";
+﻿using System.Text;
 
+namespace CDK {
+    public static class CStrings {
+        public static readonly StringBuilder Builder = new StringBuilder();
+        public const string PrefixCdkScripts = "CDK/";
+        public const string DontDestroyOnLoad = "DontDestroyOnLoad";
     }
 }
