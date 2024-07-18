e77ecd13d6be545bdd07734b867d087e6840fb4f
Chris <19819051+Chrisdbhr@users.noreply.github.com>
2024-07-18 04:41:14 -0300

    CActivatePhysicsOnDie for dynamic objects

---
+++ Scripts/Physics/CActivatePhysicsOnDie.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace CDK {
+    [RequireComponent(typeof(Rigidbody))]
+    public class CActivatePhysicsOnDie : CHealthComponent {
+
+        Rigidbody rb;
+
+
+        protected override void Awake() {
+            base.Awake();
+            TryGetComponent(out rb);
+            this.OnDie += OnDieEvent;
+        }
+
+        void OnDieEvent() {
+            rb.isKinematic = true;
+        }
+
+        void OnDestroy() {
+            this.OnDie -= OnDieEvent;
+        }
+
+        void Reset() {
+            TryGetComponent(out rb);
+            rb.isKinematic = true;
+            MaxHealth = 1f;
+        }
+
+    }
+}
