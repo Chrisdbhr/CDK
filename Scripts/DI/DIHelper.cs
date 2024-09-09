c8fea086b1a90fe71d67fcf598882d1f5a88a156
Chris <19819051+Chrisdbhr@users.noreply.github.com>
2024-09-09 16:54:14 -0300

    DIHelper returning retrieved Container

--- Scripts/DI/DIHelper.cs
+++ Scripts/DI/DIHelper.cs
@@ -1,4 +1,5 @@
-﻿using Reflex.Extensions;
+﻿using Reflex.Core;
+using Reflex.Extensions;
 using Reflex.Injectors;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -13,11 +14,16 @@ namespace CDK {
             resolvedObject = SceneManager.GetActiveScene().GetSceneContainer().Resolve<T>();
         }
 
-        public static void Inject(this GameObject gameObject) {
-            if (gameObject == null) return;
+        public static Container Inject(this GameObject gameObject) {
+            if (gameObject == null) {
+                Debug.LogError($"GameObject to inject is null, returning null Container");
+                return default;
+            }
             var scene = gameObject.scene;
             if (scene.name == CStrings.DontDestroyOnLoad) scene = SceneManager.GetActiveScene();
-            GameObjectInjector.InjectObject(gameObject, scene.GetSceneContainer());
+            var sceneContainer = scene.GetSceneContainer();
+            GameObjectInjector.InjectObject(gameObject, sceneContainer);
+            return sceneContainer;
         }
     }
 }
