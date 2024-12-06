cc7b8a7419c2576c262e1319574ae12e68b62ad3
Chris <19819051+Chrisdbhr@users.noreply.github.com>
2024-12-06 11:18:33 -0300

    quality settings only on standalone builds

--- Scripts/System/CQualitySettings.cs
+++ Scripts/System/CQualitySettings.cs
@@ -7,12 +7,15 @@ namespace CDK {
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
         static void InitializeAfterSceneLoad() {
+            #if UNITY_STANDALONE
+            Debug.Log($"{nameof(CDK)}.{nameof(CQualitySettings)}: Starting monitoring for screen resolution smaller than 1024x768 to resize.");
             Observable.EveryUpdate(CApplication.QuittingCancellationTokenSource.Token).Subscribe(_ => {
                 if (Screen.fullScreenMode == FullScreenMode.Windowed && (Screen.width < 1024 || Screen.height < 768)) {
                     Debug.Log($"{nameof(CDK)}.{nameof(CQualitySettings)}: Setting Screen Resolution to 1024x768 and fullscreen mode to false.");
                     Screen.SetResolution(1024, 768, false);
                 }
             });
+            #endif
         }
 
         public static bool GetVsync() {
