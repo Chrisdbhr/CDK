924545d7317390ff3760a35d9679913aae210f22
Chris <19819051+Chrisdbhr@users.noreply.github.com>
2024-12-19 21:10:00 -0300

    CMonoBehaviour

---
+++ Scripts/GameObjects/MonoBehaviour/CMonoBehaviour.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace CDK
+{
+    public class CMonoBehaviour : MonoBehaviour
+    {
+        protected Transform transform;
+
+        protected virtual void Awake()
+        {
+            this.transform = base.transform;
+            gameObject.Inject();
+            InjectFields();
+        }
+
+        void InjectFields()
+        {
+            InjectGetComponent();
+            InjectGetComponentInChildren();
+            InjectGetComponentInParent();
+        }
+
+        void InjectGetComponentInParent()
+        {
+            foreach (var field in GetFieldsWithAttribute(typeof(GetComponentInParentAttribute))) {
+                TrySetFieldValue(field, GetComponentInParent(field.FieldType));
+            }
+        }
+        void InjectGetComponentInChildren()
+        {
+            foreach (var field in GetFieldsWithAttribute(typeof(GetComponentInChildrenAttribute))) {
+                TrySetFieldValue(field, GetComponentInChildren(field.FieldType));
+            }
+        }
+        void InjectGetComponent()
+        {
+            foreach (var field in GetFieldsWithAttribute(typeof(GetComponentAttribute)))
+            {
+                TrySetFieldValue(field, GetComponent(field.FieldType));
+            }
+        }
+
+        IEnumerable<FieldInfo> GetFieldsWithAttribute(Type attributeType)
+        {
+            var fields = GetType()
+                .GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                .Where(field => field.GetCustomAttributes(attributeType, true).FirstOrDefault() != null);
+
+            return fields;
+        }
+
+        void TrySetFieldValue(FieldInfo field, Component component)
+        {
+            if (field.GetValue(this) != null) {
+                Debug.LogWarning($"Will not override field value of '{field.FieldType.Name}'", this);
+                return;
+            }
+            if (component == null) {
+                Debug.LogError("Component '" + field.FieldType.Name + "' in game object '" + gameObject.name + "' is null", this);
+                return;
+            }
+            field.SetValue(this, component);
+        }
+    }
+}
---
+++ Scripts/GameObjects/MonoBehaviour/MonoBehaviourAttributes.cs
@@ -0,0 +1,11 @@
+﻿namespace CDK
+{
+    [System.AttributeUsage(System.AttributeTargets.All, Inherited = false)]
+    public class GetComponentAttribute : System.Attribute { }
+
+    [System.AttributeUsage(System.AttributeTargets.All, Inherited = false)]
+    public class GetComponentInChildrenAttribute : System.Attribute { }
+
+    [System.AttributeUsage(System.AttributeTargets.All, Inherited = false)]
+    public class GetComponentInParentAttribute : System.Attribute { }
+}
