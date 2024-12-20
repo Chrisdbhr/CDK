9e2050cddd270451a4aeee1523cb4469e762b5cd
Chris <19819051+Chrisdbhr@users.noreply.github.com>
2024-12-19 21:09:50 -0300

    CAssertions

---
+++ Scripts/Extensions/CAssertions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace CDK
+{
+    public static class CAssertions
+    {
+        public static void CAssertIfFalse(this bool condition, string errorMessage = null, Object source = null)
+        {
+            if (condition) return;
+            if (errorMessage == null) {
+                Debug.LogError("Assertion failed", source);
+                return;
+            }
+
+            Debug.LogError($"Assertion failed: {errorMessage}", source);
+        }
+    }
+}
