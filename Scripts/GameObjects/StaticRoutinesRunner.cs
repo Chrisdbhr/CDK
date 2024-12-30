4d7c981b03a7532d15f711707b4a844430074656
Chris <19819051+Chrisdbhr@users.noreply.github.com>
2024-12-29 22:11:32 -0300

    StaticRoutinesRunner

---
+++ Scripts/GameObjects/StaticRoutinesRunner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace CDK
+{
+    public class StaticRoutinesRunner : MonoBehaviour
+    {
+        static StaticRoutinesRunner _instance;
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        static void Init()
+        {
+            _instance = GameObjectCreate.WithComponent<StaticRoutinesRunner>();
+        }
+
+        public static Coroutine RunRoutine(IEnumerator routine)
+        {
+            return _instance.StartCoroutine(routine);
+        }
+
+    }
+}
