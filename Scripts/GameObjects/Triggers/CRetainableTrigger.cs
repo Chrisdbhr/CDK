659830fa80b85fbbb2ae382ad255a93b76565eb2
Chris <19819051+Chrisdbhr@users.noreply.github.com>
2025-01-01 23:41:24 -0300

    CRetainableTrigger

---
+++ Scripts/GameObjects/Triggers/CRetainableTrigger.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace CDK
+{
+    public class CRetainableTrigger : MonoBehaviour
+    {
+        public CRetainable Retainable { get; } = new ();
+
+        [SerializeField] CUnityEventBool _retainedStateEvent;
+        [SerializeField] CUnityEventBool _retainedStateInverseEvent;
+        [SerializeField] UnityEvent _isRetainedEvent;
+        [SerializeField] UnityEvent _isNotRetainedEvent;
+
+
+        void OnEnable()
+        {
+            Retainable.StateEvent += RetainableOnStateEvent;
+        }
+
+        void OnDisable()
+        {
+            Retainable.StateEvent -= RetainableOnStateEvent;
+        }
+
+        void RetainableOnStateEvent(bool isRetained)
+        {
+            _retainedStateEvent?.Invoke(isRetained);
+            _retainedStateInverseEvent?.Invoke(!isRetained);
+            if (isRetained)
+            {
+                _isRetainedEvent?.Invoke();
+            }
+            else
+            {
+                _isNotRetainedEvent?.Invoke();
+            }
+        }
+    }
+}
