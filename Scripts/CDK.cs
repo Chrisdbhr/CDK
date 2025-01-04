607f6d1163d0738732e47bcb42a3ff8cf34e1419
Chris <19819051+Chrisdbhr@users.noreply.github.com>
2025-01-04 17:54:40 -0300

    5.0.3

--- Scripts/CDK.cs
+++ Scripts/CDK.cs
@@ -23,6 +23,6 @@ namespace CDK
 		{
 			Debug.Log($"CDK Version {Version}");
 		} 
-		public static readonly Version Version = new (5,0,2);
+		public static readonly Version Version = new (5,0,3);
 	}
 }
